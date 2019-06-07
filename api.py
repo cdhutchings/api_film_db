f5d003a558808350a3d087b2b6b335a120689186
Charles Hutchings <CHutchings@spartaglobal.com>
2019-06-07 11:06:32 +0100

    post request works and gives basic response. Not yet able to return a table of the request form

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,3 +10,25 @@ def index():
 @app.route('/hello')
 def hello():
     return 'Hello, World'
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+@app.route("/postform", methods=["POST"])
+def postform():
+
+    # result = request.form
+    # return render_template("result.html", result=result)
+    # CAPTURE A BODY OF JSON
+    # SORT NOUT DATA
+    # CALL MATHOD TO MAKE DATA PERSISTENT
+
+    return "Hello!"
+    pass
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      return render_template("result.html",result = result)
